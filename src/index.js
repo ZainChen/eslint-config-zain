ee108ad3b018257e9e11cf04ac8071bcc40bf8d3
ZainChen <2384439266@qq.com>
2022-10-27 19:35:33 +0800

    add eslint-plugin-simple-import-sort

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:compat/recommended',
     'plugin:prettier/recommended',
   ],
+  plugins: ['simple-import-sort'],
   env: {
     browser: true,
     node: true,
@@ -40,6 +41,27 @@ module.exports = {
           'arrow-function',
         ],
         unnamedComponents: 'function-expression',
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^@?\\w'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ['^'],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+          // 这里是把 import './*.scss' 自动排序到最后。其它排序规则顺序都保持和依赖包里 defaultGroups 一样
+          // 解决父组件样式覆盖不了子组件样式问题。如果 import './*.scss' 排在第一，子组件样式优先级会高于父组件
+          // Side effect imports.
+          ['^\\u0000'],
+        ],
       },
     ],
   },
